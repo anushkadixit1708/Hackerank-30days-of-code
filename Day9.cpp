eb91a7a61884308bfd4d89964cd04ffb5b630df9
Anushka Dixit <60402341+anushkadixit1708@users.noreply.github.com>
2021-08-03 22:05:03 +0530

    Day 9 - Recursion

---
+++ Day9.cpp
@@ -0,0 +1,70 @@
+//Complete the factorial function in the editor below. Be sure to use recursion.
+//factorial has the following paramter:
+//int n: an integer
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int factorial(int n) {
+    if(n==1){
+        return 1;
+    }
+    else {
+        return (n*factorial(n-1));
+    
+    }
+}
+
+int main()
+{
+    
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+    cin>>n;
+    factorial(n);
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
